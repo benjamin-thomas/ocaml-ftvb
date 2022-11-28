0c1645bca00b75427833d25841315218f56aafeb
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-28 18:55:23 +0100

    Formatting

--- lib/chapter02.ml
+++ lib/chapter02.ml
@@ -11,14 +11,21 @@ let by10 n = n * 10
 let both_non_zero a b = a <> 0 && b <> 0
 
 (* 3. Write a recursive function which, given a number `n`, calculates the sum 1 + 2 + 3 + ... + `n` *)
-let rec add_series n = if n = 1 then 1 else n + add_series (n - 1)
+let rec add_series n =
+  if n = 1 then
+    1
+  else
+    n + add_series (n - 1)
 
 (* 4. Write a function `power x n` wich raises x to the power n *)
-let rec power x n = if n = 0 then 1 else x * power x (n - 1)
+let rec power x n =
+  if n = 0 then
+    1
+  else
+    x * power x (n - 1)
 
 (* 5. Write a function `isconsonant` which, given a lower-case character in the range 'a'..'z',
       determines if it is a consonant *)
 let isconsonant c =
   let isvowel = 'a' = c || 'e' = c || 'i' = c || 'o' = c || 'u' = c in
   not isvowel
-;;
--- lib/chapter03.ml
+++ lib/chapter03.ml
@@ -11,11 +11,20 @@
    ========================
 *)
 
-let rec factorial n = if n = 1 then 1 else n * factorial (n - 1)
+let rec factorial n =
+  if n = 1 then
+    1
+  else
+    n * factorial (n - 1)
+
 let isvowel c = 'a' = c || 'e' = c || 'i' = c || 'o' = c || 'u' = c
 
 (* Euclid's Algorithm *)
-let rec gcd a b = if b = 0 then a else gcd b (a mod b)
+let rec gcd a b =
+  if b = 0 then
+    a
+  else
+    gcd b (a mod b)
 
 (*
    WITH PATTERN MATCHING
@@ -25,7 +34,6 @@ let rec factorial' n =
   match n with
   | 1 -> 1
   | _ -> n * factorial' (n - 1)
-;;
 
 let isvowel' c =
   match c with
@@ -34,19 +42,21 @@ let isvowel' c =
   | 'i' -> true
   | 'o' -> true
   | _ -> false
-;;
 
 let isvowel'' c =
   match c with
-  | 'a' | 'e' | 'i' | 'o' | 'u' -> true
+  | 'a'
+  | 'e'
+  | 'i'
+  | 'o'
+  | 'u' ->
+      true
   | _ -> false
-;;
 
 let rec gcd' a b =
   match b with
   | 0 -> a
   | _ -> gcd' b (a mod b)
-;;
 
 (*
    EXERCISES
@@ -58,7 +68,6 @@ let not' a =
   match a with
   | true -> false
   | false -> true
-;;
 
 (* 2. Use pattern matching to write a recursive function which, given a positive
    integer `n`, returns the sum of all integers from 1 to `n` *)
@@ -66,26 +75,22 @@ let rec sum n =
   match n with
   | 0 -> n
   | _ -> n + sum (n - 1)
-;;
 
 (* 3. Use pattern matching to write a function which, given two numbers `x` and `n`, computes `x to the power n` *)
 let rec pow x n =
   match n with
   | 0 -> 1
   | _ -> x * pow x (n - 1)
-;;
 
 let is_lower c =
   match c with
   | 'a' .. 'z' -> true
   | _ -> false
-;;
 
 let is_upper c =
   match c with
   | 'A' .. 'Z' -> true
   | _ -> false
-;;
 
 let%test _ = is_upper 'A' = true
 let%test _ = is_upper 'a' = false
