64944775835db90eea1cc5d991bede8e984375cb
Benjamin <benjamin.guy.thomas@gmail.com>
2022-11-30 21:21:09 +0100

    Document load program from file

--- lib/chapter02.ml
+++ lib/chapter02.ml
@@ -1,7 +1,21 @@
 (*
-   utop -init chapter02/main.ml
+   A) Load file on utop init
+   utop -init lib/chapter02.ml
 
-   #use "chapter02/main.ml";
+   B) Load file content into current utop
+   #use "lib/chapter02.ml";;
+
+   C) Use `.ocamlinit` to load commands on `ocaml` or `utop` init.
+   $ echo '#use "lib/chapter02.ml";;' >.ocamlinit
+   $ ocaml
+   $ utop
+
+   D) Use this `utop` alternative: https://erratique.ch/software/down/doc/manual.html
+   $ echo -e '#use "down.top"\n#use "lib/chapter02.ml";;' >.ocamlinit
+   $ ocaml
+   # Down.help ();;
+    C-x C-e  edit input with external program
+    C-t  show identifier type and documentation
 *)
 
 (* 1. Write a function which multiplies a given number by ten. What is its type. *)
@@ -16,6 +30,7 @@ let rec add_series n =
     1
   else
     n + add_series (n - 1)
+[@@ocamlformat "disable"]
 
 (* 4. Write a function `power x n` wich raises x to the power n *)
 let rec power x n =
@@ -23,6 +38,7 @@ let rec power x n =
     1
   else
     x * power x (n - 1)
+[@@ocamlformat "disable"]
 
 (* 5. Write a function `isconsonant` which, given a lower-case character in the range 'a'..'z',
       determines if it is a consonant *)
