e559568f23c3390279aaa806992e708ed2f003cc
Benjamin <benjamin.guy.thomas@gmail.com>
2023-01-25 07:55:29 +0100

    Chapter 16 WIP

---
+++ lib/chapter16/text_stats.ml
@@ -0,0 +1,59 @@
+(*
+   Text statistics
+
+   $ ocamlc ./text_stats.ml
+   $ ocaml
+   # #use "down.top";;
+   # #load "./text_stats.cmo";;
+   # Text_stats.stats_of_file "../chapter13.txt";;
+   - : int * int * int * int = (8, 461, 77, 4)
+
+ *)
+
+let hello = 12
+
+type stats = int * int * int * int
+
+let lines (l, _, _, _) = l
+let chars (_, c, _, _) = c
+let words (_, _, w, _) = w
+let sentences (_, _, _, s) = s
+
+let stats_of_channel ic =
+  let lines = ref 0 in
+  let chars = ref 0 in
+  let words = ref 0 in
+  let sentences = ref 0 in
+  try
+    while true do
+      let line = input_line ic in
+      lines := !lines + 1
+      ; chars := !chars + String.length line
+      ; String.iter
+          (fun c ->
+            match c with
+            | '.'
+            | '?'
+            | '!' ->
+                sentences := !sentences + 1
+            | ' ' -> words := !words + 1
+            | _ -> ())
+          line
+    done
+    ; (0, 0, 0, 0)
+  with
+  | End_of_file -> (!lines, !chars, !words, !sentences)
+;;
+
+let stats_of_file path =
+  let chan = open_in path in
+  let stats = stats_of_channel chan in
+  close_in chan
+  ; stats
+;;
+
+(* let%test_unit _ =
+     let open Base in
+     let path = "../../../../../lib/chapter13.txt" in
+     [%test_eq: int * int * int * int] (stats_of_file path) @@ (8, 461, 77, 4)
+   ;; *)
