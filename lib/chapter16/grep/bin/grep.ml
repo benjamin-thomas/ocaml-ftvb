bdd2a5bee139d1827fc3ab569194f0a836ab06d6
Benjamin <benjamin.guy.thomas@gmail.com>
2023-01-29 11:38:56 +0100

    Grep: basic scaffolding

---
+++ lib/chapter16/grep/bin/grep.ml
@@ -0,0 +1,4 @@
+(*
+  See ./lib/grep_lib.ml for run instructions
+*)
+let () = Grep_lib.hello |> print_endline
---
+++ lib/chapter16/grep/lib/grep_lib.ml
@@ -0,0 +1,10 @@
+(*
+
+  Write a standalone program to search for a given string in a file.
+  Lines where the string is found should be printed to the screen.
+
+  dune exec --display=quiet ./bin/grep.exe ABC /tmp/tmp
+
+*)
+
+let hello = "world"
